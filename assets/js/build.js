fc3395ba06fd73517ee3c712de1953051ab471b0
Wendy <wendy@thoughtcatalog.com>
2017-07-25 18:35:25 -0400

    add auto play to single video page

--- assets/js/build.js
+++ assets/js/build.js
@@ -1,35 +1,60 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-;(function (window, document, $, undefined) {
+var _setupPlayer = require('./setup-player');
 
-  var id = 'jw-player-showcase',
-      player = null,
-      video = null;
+var _setupPlayer2 = _interopRequireDefault(_setupPlayer);
 
-  document.addEventListener('DOMContentLoaded', function (event) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-    init();
-  });
+(0, _setupPlayer2.default)();
 
-  function init() {
+},{"./setup-player":2}],2:[function(require,module,exports){
+'use strict';
 
-    player = document.getElementById(id);
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var player_id = 'jw-player-showcase',
+    single_video_class = 'jw-player-showcase-single-video',
+    player = null,
+    video = null,
+    single_video = [];
 
-    if (!player) {
-      return;
-    }
+function load_player() {
 
-    setup_video();
-  }
+	document.addEventListener('DOMContentLoaded', function (event) {
 
-  function setup_video() {
+		init_player();
+	});
+}
 
-    video = JSON.parse(player.getAttribute('data-video'));
+function init_player() {
 
-    jwplayer(id).setup(video);
-  }
-})(window, document, jQuery);
+	player = document.getElementById(player_id);
+
+	if (!player) {
+		return;
+	}
+
+	setup_video();
+}
+
+function setup_video() {
+
+	video = JSON.parse(player.getAttribute('data-video'));
+
+	single_video = document.getElementsByClassName(single_video_class);
+
+	if (0 !== single_video.length) {
+
+		video["autostart"] = true;
+	}
+
+	jwplayer(player_id).setup(video);
+}
+
+exports.default = load_player;
 
 },{}]},{},[1])
 
